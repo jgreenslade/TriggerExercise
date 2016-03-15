a298e15c667ab38d4b1761a73c6658dd3496537a
jgreenslade <jgreenslade@elon.edu>
2016-03-15 17:02:19 -0400

    Added Project Folder

---
+++ New Unity Project/Assets/RetroPC/Scripts/ComputerInteractScript.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ComputerInteractScript : MonoBehaviour {
+
+    public Canvas canvas;
+
+    public Canvas canvas2;
+
+    private Text textComponent;
+
+    private bool interacting;
+
+    private string[] msg = { "Press X to interact with this Computer.", "Congrats!\n The code is 1234. " };
+
+    /// <summary>
+    /// Later just make an array of all the things to be displayed
+    /// </summary>
+
+	// Use this for initialization
+	void Start () {
+        canvas.enabled = false;
+        canvas2.enabled = false;
+        interacting = false;
+
+        textComponent = canvas.transform.FindChild("InteractText").gameObject.GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (interacting) {
+            canvas.enabled = false;
+            canvas2.enabled = true;
+            //textComponent.text = msg[1];
+        }
+	}
+
+    void OnTriggerEnter(Collider otherCollider) {
+        GameObject other = otherCollider.gameObject;
+        if (other.tag == "Player")
+        {
+            canvas.enabled = true;
+
+        }
+    }
+
+    void OnTriggerStay(Collider otherCollider)
+    {
+        GameObject other = otherCollider.gameObject;
+        if (other.tag == "Player")
+        {
+            // Check for input
+            if (Input.GetKey(KeyCode.X))
+            {
+                interacting = true;
+                Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider otherCollider)
+    {
+        GameObject other = otherCollider.gameObject;
+        if (other.tag == "Player")
+        {
+            interacting = false;
+            canvas.enabled = false;
+            canvas2.enabled = false;
+        }
+    }
+}
---
+++ New Unity Project/Assets/RetroPC/Scripts/DoorInteractScript.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DoorInteractScript : MonoBehaviour {
+
+    public Canvas canvas;
+
+    public InputField codeInputField;
+
+    private bool interacting;
+
+    private string[] msg = { "Press X to interact with this Computer.", "Congrats!\n The code is 1234. " };
+    private Material mat;
+    /// <summary>
+    /// Later just make an array of all the things to be displayed
+    /// </summary>
+
+	// Use this for initialization
+	void Start () {
+        canvas.enabled = false;
+        interacting = false;
+
+        // Add listener for submit value
+        InputField.SubmitEvent submitEvent = new InputField.SubmitEvent();
+        submitEvent.AddListener(submitValue);
+        codeInputField.onEndEdit = submitEvent;
+
+        mat = this.gameObject.GetComponent<Renderer>().material;
+    }
+
+    void submitValue(string code) {
+        Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        if (code.Equals("1234")) {
+            if (mat != null)
+            {
+                mat.color = Color.green;
+            }
+            else {
+                Debug.Log("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+            }
+            }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+	}
+
+    void OnTriggerEnter(Collider otherCollider) {
+        GameObject other = otherCollider.gameObject;
+        if (other.tag == "Player")
+        {
+            canvas.enabled = true;
+
+        }
+    }
+
+    void OnTriggerStay(Collider otherCollider)
+    {
+        GameObject other = otherCollider.gameObject;
+
+    }
+
+    void OnTriggerExit(Collider otherCollider)
+    {
+        GameObject other = otherCollider.gameObject;
+        if (other.tag == "Player")
+        {
+            interacting = false;
+            canvas.enabled = false;
+        }
+    }
+}
